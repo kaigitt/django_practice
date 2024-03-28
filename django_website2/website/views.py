925d553358804cf3ba668ac75ba461c8338d8c13
kaito <s12201600350@gmail.com>
2024-03-28 17:46:22 +0900

    practice

---
+++ django_website2/website/views.py
@@ -0,0 +1,23 @@
+from django.views.generic import TemplateView
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self):
+        ctxt = super().get_context_data()
+        ctxt["username"] = ""
+        return ctxt
+
+class AboutView(TemplateView):
+    template_name = "about.html"
+
+    def get_context_data(self):
+        ctxt = super().get_context_data()
+        ctxt["num_services"] = "123456789"
+        ctxt["skills"] = [
+            "Python",
+            "C",
+            "Rust"
+        ]
+        return ctxt
+
